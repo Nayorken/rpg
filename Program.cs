e53d6f7108573e94711f6de02d1ce0cfb0d63ec1
nayorken <hugo.ferreira1617@gmail.com>
2019-11-17 21:56:03 +0000

    up 17

--- Program.cs
+++ Program.cs
@@ -4,9 +4,59 @@ namespace rpg_game
 {
     class Program
     {
+        
+        private fechar fecha = new fechar();
+        
+        
+        
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Estás pronto para o desafio");
+            Console.WriteLine("\ts - Sim");
+            Console.WriteLine("\tn - Não");
+
+
+            switch (Console.ReadLine())
+            {
+                case "s":
+                    Console.WriteLine("Muito bem, antes de começarmos vamos verificar se é humano quanto é 2x4?");
+                    break;
+
+                case "n":
+                    Console.WriteLine("Que pena espero que voltes com mais vontade para a proxima!");
+                    Console.ReadKey();
+                    
+                    break;
+            }
+
+            Console.WriteLine("Estás pronto para o desafio");
+            Console.WriteLine("\t2 - 2");
+            Console.WriteLine("\t5 - 5");
+            Console.WriteLine("\t8 - 8");
             
+            switch (Console.ReadLine())
+            {
+                case "8":
+                    Console.WriteLine("Parabens tens mais iq ");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Vai estudar");
+                    Console.WriteLine("Game over!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+                case "5":
+                    Console.WriteLine("Vai estudar");
+                    Console.WriteLine("Game Over!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ fechar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rpg_game
+{
+    class fechar
+    {
+        Console.WriteLine("Estás pronto para o desafio");
+    }
+}
